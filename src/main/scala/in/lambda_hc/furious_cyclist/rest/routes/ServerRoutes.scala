ce4eb0ca42d53ea03bb0d6e9e91e3242a0e34491
Vishnu Prasad <vishnu667@gmail.com>
2016-06-19 08:27:20 +0530

    Set Redirect Path for the Api

--- src/main/scala/in/lambda_hc/furious_cyclist/rest/routes/ServerRoutes.scala
+++ src/main/scala/in/lambda_hc/furious_cyclist/rest/routes/ServerRoutes.scala
@@ -3,7 +3,7 @@ package in.lambda_hc.furious_cyclist.rest.routes
 import com.google.inject.Inject
 import in.lambda_hc.furious_cyclist.rest.handlers.DefaultApiHandler
 import io.undertow.server.HttpHandler
-import io.undertow.server.handlers.PathHandler
+import io.undertow.server.handlers.{RedirectHandler, PathHandler}
 
 /**
   * Created by vishnu on 9/6/16.
@@ -16,7 +16,7 @@ class ServerRoutes @Inject()(
 
   def getAllHandlers: HttpHandler = {
     new PathHandler()
-      .addExactPath("/", defaultApiHandler)
+      .addExactPath("/", new RedirectHandler("https://github.com/lambda-hc/furious-cyclist/blob/master/API-Specs.md"))
       .addPrefixPath("api", apiRoutes.pathHandler)
   }
 
